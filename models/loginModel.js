18c9947f6f30661616e8bcb88cdf8fa8785dc353
Andrew Martins <andrew_omartins@yahoo.com>
2021-05-18 23:01:44 -0400

    implemeted hash method for bycrypt

--- models/loginModel.js
+++ models/loginModel.js
@@ -2,3 +2,45 @@
 
 const db = require("./conn"),
     bycrypt = require("bcryptjs")
+
+    class Login {
+        constructor(id, username, password) {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+
+        //Checks that the username is unique
+        static async checkUserNames(username) {
+            try {
+              const response = await db.any(
+                `SELECT username FROM volunteers WHERE username='${username}';`
+              );
+              return response;
+            } catch (err) {
+              return false;
+            }
+          }
+
+
+
+          checkPassword(hashedPassword) {
+            return bcrypt.compareSync(this.password, hashedPassword);
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
